131c8d8a79281dc7e8d7d708756cf6dcf79d03ad
dudaskobende <dudaskobende@gmail.com>
2022-08-10 14:22:45 +0200

    chore: initialize project

---
+++ src/main/java/com/codecool/terraformingmarsforum/TerraformingMarsForumApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.terraformingmarsforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformingMarsForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraformingMarsForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/terraformingmarsforum/TerraformingMarsForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.terraformingmarsforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformingMarsForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
