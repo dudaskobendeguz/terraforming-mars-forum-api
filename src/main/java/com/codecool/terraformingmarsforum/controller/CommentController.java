b10c630ecf048d9293177167d8aab315ae710c5a
benedekhalaj <bendihalaj@gmail.com>
2022-08-11 18:17:15 +0200

    feat: change CommentController requestMapping and add createComment request

--- src/main/java/com/codecool/terraformingmarsforum/controller/CommentController.java
+++ src/main/java/com/codecool/terraformingmarsforum/controller/CommentController.java
@@ -1,9 +1,26 @@
 package com.codecool.terraformingmarsforum.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.terraformingmarsforum.model.Comment;
+import com.codecool.terraformingmarsforum.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
-@RequestMapping("/api/comment")
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("")
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) throws URISyntaxException {
+        int id = commentService.createComment(comment);
+        return ResponseEntity.created(new URI(String.format("/api/comments/%d", id))).body(comment);
+    }
 }
