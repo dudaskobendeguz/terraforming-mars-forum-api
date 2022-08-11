ca98d10cf77aa547f7523462abb7af4e9f71694f
dudaskobende <dudaskobende@gmail.com>
2022-08-11 11:52:56 +0200

    refactor: rename user route, and rename user nickname field

--- src/main/java/com/codecool/terraformingmarsforum/model/User.java
+++ src/main/java/com/codecool/terraformingmarsforum/model/User.java
@@ -10,6 +10,6 @@ public class User {
     private int id;
     private String name;
     private String password;
-    private String nickName;
+    private String nickname;
     private String imgUrl;
 }
