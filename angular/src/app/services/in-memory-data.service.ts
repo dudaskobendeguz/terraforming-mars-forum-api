c97eb446f75a2382b884d8e6144e6ee61cbea8e5
sagi-viktor <sagi.viktor1@gmail.com>
2022-08-23 19:41:13 +0200

    chore: add in-memory sample data

--- angular/src/app/services/in-memory-data.service.ts
+++ angular/src/app/services/in-memory-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {InMemoryDbService} from "angular-in-memory-web-api";
 import {User} from "../interfaces/user";
-import {Observable} from "rxjs";
+import {Comment} from "../interfaces/comment";
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +17,23 @@ export class InMemoryDataService implements InMemoryDbService{
         email: "bende@gmail.com",
         firstName: "Hello",
         lastName: "user",
-        imageSource: "img",
+        imageSource: "https://xsgames.co/randomusers/assets/images/favicon.png",
         username: "bende",
         password: "1234"
       }
     ]
-    return {users};
+    const comments: Comment[] = [
+      {
+        id: 1,
+        user: users[0],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut" +
+          "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut" +
+          "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum" +
+          "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia" +
+          "deserunt mollit anim id est laborum.",
+        timestamp: new Date()
+      }
+    ]
+    return {users, comments};
   }
 }
