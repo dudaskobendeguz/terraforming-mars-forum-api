c61bd1b286a6535e35b6cc1dfbc11f727e48b5aa
benedekhalaj <bendihalaj@gmail.com>
2022-08-23 20:27:07 +0200

    feat: add Home and User Posts navbar links

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserPostContainerComponent} from "./components/user-post-container/user-post-container.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user-posts', component: UserPostContainerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
