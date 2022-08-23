ed950ad3419d80119fc608998538b2578450528a
dudaskobende <dudaskobende@gmail.com>
2022-08-23 21:28:17 +0200

    chore: add league posts link

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserPostContainerComponent} from "./components/user-post-container/user-post-container.component";
+import {
+  LeaguePostContainerComponent
+} from "./components/league-posts/league-post-container/league-post-container.component";
 
 const routes: Routes = [
-  {path: 'user-posts', component: UserPostContainerComponent}
+  {path: 'user-posts', component: UserPostContainerComponent},
+  {path: 'league-posts', component: LeaguePostContainerComponent}
 ];
 
 @NgModule({
