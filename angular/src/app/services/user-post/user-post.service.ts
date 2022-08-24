61631ea45db725366a5f8d388e71d4f16f04cd43
benedekhalaj <bendihalaj@gmail.com>
2022-08-24 14:41:02 +0200

    chore: move user post id generator to User Post Service

--- angular/src/app/services/user-post/user-post.service.ts
+++ angular/src/app/services/user-post/user-post.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {MessageLoggerService} from "../message-logger/message-logger.service";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ErrorHandlerService} from "../error-handler/error-handler.service";
@@ -14,11 +14,15 @@ export class UserPostService {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
+  private generateId: number = 100;
+
+
   constructor(
     private messageLogger: MessageLoggerService,
     private http: HttpClient,
     private errorHandler: ErrorHandlerService
-  ) { }
+  ) {
+  }
 
   getUserPosts(): Observable<UserPost[]> {
     return this.http.get<UserPost[]>(this.userPostsUrl)
@@ -29,10 +33,21 @@ export class UserPostService {
   }
 
   addUserPost(userPost: UserPost): Observable<UserPost> {
+    // Generate ID
+    userPost.id = ++this.generateId;
     return this.http.post<UserPost>(this.userPostsUrl, userPost, this.httpOptions)
       .pipe(
-        tap((_) => this.log(`created user post with id=${userPost.id}`)),
+        tap((_) => this.log(`create user post with id=${userPost.id} (TAP)`)),
         catchError(this.errorHandler.handleError<UserPost>('addUserPost'))
+      );
+  }
+
+  deleteUserPost(userPost: UserPost): Observable<UserPost> {
+    const url = `${this.userPostsUrl}/${userPost.id}`;
+    return this.http.delete<UserPost>(url, this.httpOptions)
+      .pipe(
+        tap((_) => this.log(`delete user post with id=${userPost.id} (TAP)`)),
+        catchError(this.errorHandler.handleError<UserPost>("deleteUserPost"))
       );
   }
 
