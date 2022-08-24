7e27da11d8a7316e24bde2e1cf75a02acf99e1b6
dudaskobende <dudaskobende@gmail.com>
2022-08-24 14:10:54 +0200

    chore: modify league post

--- angular/src/app/services/in-memory-data.service.ts
+++ angular/src/app/services/in-memory-data.service.ts
@@ -66,27 +66,21 @@ export class InMemoryDataService implements InMemoryDbService{
       {
         id: 1,
         timestamp: new Date(),
-        description: "Im a league post description",
+        gameType: "Ares Expedition",
+        name: "My Test League",
+        leagueAdmin: users[0],
+        numberOfRounds: 2,
         imageSource: "https://bobbyhadz.com/images/blog/typescript-date-format/banner.webp",
-        leagueDetails: {id: 1},
         comments: comments
       },
       {
         id: 2,
         timestamp: new Date(),
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
-          "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
-          "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
-          "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
-          "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
-          "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
-          "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
-          "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
-          "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
-          "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis \n" +
-          "minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit ",
+        gameType: "Terraforming Mars",
+        name: "Test Mars League",
+        leagueAdmin:users[1],
+        numberOfRounds: 4,
         imageSource: "https://raketa.hu/uploads/2022/07/mars_sample_return-768x432.jpg",
-        leagueDetails: {id: 2},
         comments: comments
       }
     ]
