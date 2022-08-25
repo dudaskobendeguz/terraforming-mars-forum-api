89d325fd0acc7df4969237f3251e29f669511641
dudaskobende <dudaskobende@gmail.com>
2022-08-25 14:47:45 +0200

    chore: add get league post by league id

--- angular/src/app/components/league-posts/league-pots-detail/league-pots-detail.component.ts
+++ angular/src/app/components/league-posts/league-pots-detail/league-pots-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LeagueDetails} from "../../../interfaces/league-details";
+import {MessageLoggerService} from "../../../services/message-logger/message-logger.service";
+import {LeaguePostDetailService} from "../../../services/league-post-detail/league-post-detail.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-league-pots-detail',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./league-pots-detail.component.css']
 })
 export class LeaguePotsDetailComponent implements OnInit {
+  postDetails?: LeagueDetails;
 
-  constructor() { }
+  constructor(
+    private messageService: MessageLoggerService,
+    private postDetailService: LeaguePostDetailService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.getPostDetailById();
   }
 
+  private getPostDetailById() {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.postDetailService.getByLeagueId(id).subscribe( (postDetails) => {
+      this.postDetails = postDetails;
+      this.messageService.add(LeaguePostDetailService.name, `getPostDetailsById(${id}) fetched post details`);
+    });
+  }
 }
