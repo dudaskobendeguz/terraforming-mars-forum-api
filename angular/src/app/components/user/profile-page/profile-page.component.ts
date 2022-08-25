654765651af2476f78ae01d7e4990acc429886d0
Zsezsu <zsezsu6@gmail.com>
2022-08-25 12:33:00 +0200

    chore: add messageLogger private field to profile-page component

--- angular/src/app/components/user/profile-page/profile-page.component.ts
+++ angular/src/app/components/user/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../../interfaces/user";
 import {UserService} from "../../../services/user/user.service";
+import {MessageLoggerService} from "../../../services/message-logger/message-logger.service";
 
 @Component({
   selector: 'app-profile-page',
@@ -10,12 +11,13 @@ import {UserService} from "../../../services/user/user.service";
 export class ProfilePageComponent implements OnInit {
   @Input() user?: User;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private messageLogger: MessageLoggerService) { }
 
   ngOnInit(): void {
     this.userService.getUserById('1')
       .subscribe((user) => {
         this.user = user;
+        this.messageLogger.add(ProfilePageComponent.name, 'getUserById: user fetched')
       });
   }
 
