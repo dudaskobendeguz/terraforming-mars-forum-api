a574701bbbcb75cc075f6e00dc714d2d8134af3c
dudaskobende <dudaskobende@gmail.com>
2022-08-25 15:20:17 +0200

    chore: add post details to post

--- angular/src/app/components/league-posts/league-pots-detail/league-pots-detail.component.ts
+++ angular/src/app/components/league-posts/league-pots-detail/league-pots-detail.component.ts
@@ -3,6 +3,8 @@ import {LeagueDetails} from "../../../interfaces/league-details";
 import {MessageLoggerService} from "../../../services/message-logger/message-logger.service";
 import {LeaguePostDetailService} from "../../../services/league-post-detail/league-post-detail.service";
 import {ActivatedRoute} from "@angular/router";
+import {LeaguePost} from "../../../interfaces/league-post";
+import {LeaguePostService} from "../../../services/league-post/league-post.service";
 
 @Component({
   selector: 'app-league-pots-detail',
@@ -11,22 +13,32 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class LeaguePotsDetailComponent implements OnInit {
   postDetails?: LeagueDetails;
+  leaguePost?: LeaguePost;
 
   constructor(
     private messageService: MessageLoggerService,
     private postDetailService: LeaguePostDetailService,
+    private leaguePostService: LeaguePostService,
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.getPostDetailById();
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    this.getPostByiD(id);
+    this.getPostDetailById(id);
   }
 
-  private getPostDetailById() {
-    const id: number = Number(this.route.snapshot.paramMap.get('id'));
-    this.postDetailService.getByLeagueId(id).subscribe( (postDetails) => {
-      this.postDetails = postDetails;
+  private getPostDetailById(id: number): void {
+    this.postDetailService.getByLeagueId(id).subscribe( (leagueDetails) => {
+      this.postDetails = leagueDetails;
       this.messageService.add(LeaguePostDetailService.name, `getPostDetailsById(${id}) fetched post details`);
+    });
+  }
+
+  private getPostByiD(id: number): void {
+    this.leaguePostService.getLeaguePostById(id).subscribe( (leaguePost) => {
+      this.leaguePost = leaguePost;
+      this.messageService.add(LeaguePostDetailService.name, `getPostById(${id}) fetched post`);
     });
   }
 }
