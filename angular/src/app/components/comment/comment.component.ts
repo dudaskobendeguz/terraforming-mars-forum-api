3d6e1ffe21b97d00b2d407db9861331693816a7d
Benedek Halaj <bendihalaj@gmail.com>
2022-08-29 19:31:19 +0200

    feat: add comment component design

--- angular/src/app/components/comment/comment.component.ts
+++ angular/src/app/components/comment/comment.component.ts
@@ -9,8 +9,14 @@ import {PostComment} from "../../interfaces/post-comment";
 export class CommentComponent implements OnInit {
 
   @Input() comment?: PostComment
+  date?: string
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.comment) {
+      const date = new Date(this.comment.timestamp);
+      this.date = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getUTCMinutes()}`
+    }
+  }
 }
