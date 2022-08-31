257e0cc3dce74303f131ccfa39df0991edbdbaaf
dudaskobende <dudaskobende@gmail.com>
2022-08-31 10:46:51 +0200

    chore: add 'date ago' component logic

--- angular/src/app/components/date-ago/date-ago.component.ts
+++ angular/src/app/components/date-ago/date-ago.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-date-ago',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-ago.component.css']
 })
 export class DateAgoComponent implements OnInit {
+  @Input() date?: Date;
 
   constructor() { }
 
