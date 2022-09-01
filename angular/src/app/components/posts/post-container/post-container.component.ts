616b474f1250ce37b83ed8af20b49bd5852c7840
dudaskobende <dudaskobende@gmail.com>
2022-09-01 15:53:44 +0200

    chore: add home page show posts by date order

--- angular/src/app/components/posts/post-container/post-container.component.ts
+++ angular/src/app/components/posts/post-container/post-container.component.ts
@@ -28,7 +28,8 @@ export class PostContainerComponent implements OnInit {
     this.userPostService.getUserPosts().subscribe((userPosts) => {
       userPosts.forEach((userPost) => this.posts.push({userPost: userPost}));
       this.log('getAllUserPost: all user post added to "posts" array');
-      this.posts = arrayShuffle(this.posts);
+      this.filterByDate(true);
+      // this.posts = arrayShuffle(this.posts);
     });
   }
 
@@ -36,11 +37,20 @@ export class PostContainerComponent implements OnInit {
     this.leaguePostService.getLeaguePosts().subscribe((leaguePosts) => {
       leaguePosts.forEach((leaguePost) => this.posts.push({leaguePost: leaguePost}));
       this.log('getAllLeaguePost: all league post added to "posts" array');
-      this.posts = arrayShuffle(this.posts);
+      this.filterByDate(true);
+      // this.posts = arrayShuffle(this.posts);
     })
   }
 
   private log(message: string): void {
     this.logger.add(PostContainerComponent.name, message);
+  }
+
+  private filterByDate(isAscending: boolean): void {
+    this.posts.sort((post1, post2) => {
+      const date1: Date = (post1.userPost?.timestamp) ? post1.userPost.timestamp : (post1.leaguePost?.timestamp) ? post1.leaguePost.timestamp : new Date();
+      const date2: Date = (post2.userPost?.timestamp) ? post2.userPost.timestamp : (post2.leaguePost?.timestamp) ? post2.leaguePost.timestamp : new Date();
+      return isAscending ? (date1 < date2 ? 1 : -1) : (date2 < date1 ? 1 : -1);
+    });
   }
 }
