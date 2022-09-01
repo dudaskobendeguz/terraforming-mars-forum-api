1bc48109d277f3a32fb3403bf083eb0481d53e40
dudaskobende <dudaskobende@gmail.com>
2022-09-01 13:19:43 +0200

    chore: add documentation to 'post header' component

--- angular/src/app/components/league-posts/post-header/post-header.component.ts
+++ angular/src/app/components/league-posts/post-header/post-header.component.ts
@@ -1,6 +1,16 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../../interfaces/user";
 
+
+/**
+ * Generate post header component
+ * @param {string} title - the title of the header(will be in titlecase)
+ * @param {string} titleLink - OPTIONAL The title can be a link. In this case, titleLink needs ie.: "/user/12"
+ * @param {string} subtitle - OPTIONAL the subtitle of the header
+ * @param {date} date - will show in subtitle
+ * @param {User} user - the owner of the post. the avatar will be a link to the user profile
+ *
+ */
 @Component({
   selector: 'app-post-header',
   templateUrl: './post-header.component.html',
@@ -8,11 +18,10 @@ import {User} from "../../../interfaces/user";
 })
 export class PostHeaderComponent implements OnInit {
   @Input() title?: string;
+  @Input() titleLink?: string;
   @Input() subtitle?: string;
   @Input() date?: Date;
-  @Input() avatarImageSource?: string;
   @Input() user?: User;
-  @Input() titleLink?: string;
 
   constructor() { }
 
