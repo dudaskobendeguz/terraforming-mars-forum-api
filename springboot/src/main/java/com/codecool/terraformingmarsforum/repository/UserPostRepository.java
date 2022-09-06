4632fdc563745ab319978549cedb8d81b8bf764a
Benedek Halaj <bendihalaj@gmail.com>
2022-09-06 18:02:46 +0200

    chore: add Repository annotation to UserPostRepository

--- springboot/src/main/java/com/codecool/terraformingmarsforum/repository/UserPostRepository.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/repository/UserPostRepository.java
@@ -2,7 +2,9 @@ package com.codecool.terraformingmarsforum.repository;
 
 import com.codecool.terraformingmarsforum.model.UserPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserPostRepository extends JpaRepository<UserPost, Long> {
 
 }
