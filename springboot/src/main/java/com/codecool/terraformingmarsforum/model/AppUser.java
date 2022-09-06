bc9b7a6db94e46a1cb344016bd415bc7432f8fee
Zsezsu <zsezsu6@gmail.com>
2022-09-06 13:55:08 +0200

    feat: add AppUser Entity

--- springboot/src/main/java/com/codecool/terraformingmarsforum/model/AppUser.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/model/AppUser.java
@@ -1,15 +1,47 @@
 package com.codecool.terraformingmarsforum.model;
 
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@Entity
+@RequiredArgsConstructor
 public class AppUser {
-    private int id;
-    private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
     private String password;
-    private String nickname;
-    private String imgUrl;
+    private String imgSource;
+    private Date timestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AppUser appUser = (AppUser) o;
+        return id != null && Objects.equals(id, appUser.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
