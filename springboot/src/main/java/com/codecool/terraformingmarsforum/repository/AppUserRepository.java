9c88c1d0a418ad1c3ad5d52434736f393e1ee93a
Zsezsu <zsezsu6@gmail.com>
2022-09-07 19:51:53 +0200

    chore: add findAppUserByUsernameOrEmail() to AppUserRepository

--- springboot/src/main/java/com/codecool/terraformingmarsforum/repository/AppUserRepository.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/repository/AppUserRepository.java
@@ -4,6 +4,10 @@ import com.codecool.terraformingmarsforum.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findAppUserByUsernameOrEmail(String username, String email);
 }
