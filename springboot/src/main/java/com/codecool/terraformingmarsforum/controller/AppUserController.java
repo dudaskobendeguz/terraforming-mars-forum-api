12d42ce2e1cf025cf047d9a17777b6869a450428
Zsezsu <zsezsu6@gmail.com>
2022-09-07 17:36:37 +0200

    chore: add AppUserService field to AppUserController

--- springboot/src/main/java/com/codecool/terraformingmarsforum/controller/AppUserController.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/controller/AppUserController.java
@@ -1,13 +1,16 @@
 package com.codecool.terraformingmarsforum.controller;
 
 
+import com.codecool.terraformingmarsforum.service.AppUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class AppUserController {
 
-
+    private final AppUserService appUserService;
 
 }
