475dba13db909ccff5d06c7a331a033e252d0b43
Zsezsu <zsezsu6@gmail.com>
2022-09-07 18:23:27 +0200

    feat: implement getUserById in AppUserController

--- springboot/src/main/java/com/codecool/terraformingmarsforum/controller/AppUserController.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/controller/AppUserController.java
@@ -1,10 +1,16 @@
 package com.codecool.terraformingmarsforum.controller;
 
 
+import com.codecool.terraformingmarsforum.model.AppUser;
 import com.codecool.terraformingmarsforum.service.AppUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/users")
@@ -12,5 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppUserController {
 
     private final AppUserService appUserService;
+    private final RestExceptionHandler restExceptionHandler;
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable Long id){
+        try {
+            AppUser appUser = appUserService.getAppUserById(id);
+            return ResponseEntity.ok(appUser);
+        } catch (NoSuchElementException msg){
+            return restExceptionHandler.handleNoSuchElementException();
+        }
+    }
 
 }
