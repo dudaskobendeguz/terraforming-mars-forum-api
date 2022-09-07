176ff001dd9d1c9b740ad6892385240ff468595c
Benedek Halaj <bendihalaj@gmail.com>
2022-09-07 08:29:43 +0200

    feat: add global exception handler

---
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/controller/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.codecool.terraformingmarsforum.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<String> handleNoSuchElementException() {
+        return ResponseEntity.notFound().build();
+    }
+}
