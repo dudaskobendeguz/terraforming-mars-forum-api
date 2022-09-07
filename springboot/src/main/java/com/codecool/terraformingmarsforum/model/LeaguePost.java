1c8fb66f27ce2f5d8fe04c6db61c5d46a3431937
dudaskobende <dudaskobende@gmail.com>
2022-09-07 22:09:43 +0200

    chore: add 'league status' field and create 'description' method to 'league post' entity

--- springboot/src/main/java/com/codecool/terraformingmarsforum/model/LeaguePost.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/model/LeaguePost.java
@@ -1,7 +1,9 @@
 package com.codecool.terraformingmarsforum.model;
 
+import com.codecool.terraformingmarsforum.model.types.LeagueStatus;
 import lombok.*;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.*;
 import java.util.List;
 
@@ -27,9 +29,46 @@ public class LeaguePost {
 
     //TODO Generate description based on the current league status.
     String description;
+
+    LeagueStatus leagueStatus;
+
     @ManyToOne
     LeagueDetail leagueDetail;
 
     @OneToMany
     List<Comment> comments;
+
+    @PostConstruct
+    private void createDescription() {
+        switch (leagueStatus) {
+            case STARTED ->
+                description = String.format(
+                        "%s has started a %s %s league!",
+                        leagueDetail.getLeagueAdmin().getUsername(),
+                        leagueDetail.gameType,
+                        leagueDetail.name
+                );
+
+            case ROUND_IN_PROGRESS ->
+                description = String.format(
+                        "Round %d started!",
+                        (leagueDetail.numberOfFinishedRounds + 1)
+                );
+
+            case ROUND_FINISHED ->
+                description = String.format(
+                        "Round %d/%d finished",
+                        leagueDetail.numberOfFinishedRounds,
+                        leagueDetail.numberOfRounds
+                );
+
+            case FINISHED ->
+                description = String.format(
+                        "The %s %s league has finished!",
+                        leagueDetail.gameType,
+                        leagueDetail.name
+                );
+            default -> throw new IllegalArgumentException("No description for the given LeagueStatus");
+        }
+    }
 }
