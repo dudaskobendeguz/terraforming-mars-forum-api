7f9f3b9063fc4449a8bd859302bdd6e71261b254
Zsezsu <zsezsu6@gmail.com>
2022-09-07 18:21:12 +0200

    chore: implement getAppUserById() in AppUserService

--- springboot/src/main/java/com/codecool/terraformingmarsforum/service/AppUserService.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/service/AppUserService.java
@@ -1,14 +1,19 @@
 package com.codecool.terraformingmarsforum.service;
 
-
+import com.codecool.terraformingmarsforum.model.AppUser;
 import com.codecool.terraformingmarsforum.repository.AppUserRepository;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class AppUserService {
 
     private final AppUserRepository appUserRepository;
+
+    public AppUser getAppUserById(Long id) {
+        return appUserRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
 }
