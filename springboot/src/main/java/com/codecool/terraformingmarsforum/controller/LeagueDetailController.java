1c2d165bb7ac3336f0b5e4e00603e8e039376667
dudaskobende <dudaskobende@gmail.com>
2022-09-08 16:36:42 +0200

    chore: add 'create league detail' method to 'league' DTO

--- springboot/src/main/java/com/codecool/terraformingmarsforum/controller/LeagueDetailController.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/controller/LeagueDetailController.java
@@ -1,14 +1,18 @@
 package com.codecool.terraformingmarsforum.controller;
 
 
+import com.codecool.terraformingmarsforum.model.AppUser;
+import com.codecool.terraformingmarsforum.model.LeagueDetail;
 import com.codecool.terraformingmarsforum.service.LeagueDetailService;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,61 +22,119 @@ public class LeagueDetailController {
 
 
     /**
-     * Representation of the "player" of the {@link League leage}'s players,
-     * who's are Users of the <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League</a>
+     * Representation of the {@link League leage}'s players,
+     * who's are Users of the
+     * <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League</a>
      */
     @Data
     private static class Player {
+        /**
+         * The id of the
+         * <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League's</a> user
+         */
+        @NonNull
         @JsonProperty("id")
         Long id;
 
+        @NonNull
         @JsonProperty("email")
         String email;
 
+        @NonNull
         @JsonProperty("first_name")
         String firstName;
 
+        @NonNull
         @JsonProperty("last_name")
         String lastName;
 
+        @NonNull
         @JsonProperty("image_source")
         String imageSource;
 
+        @NonNull
         @JsonProperty("username")
         String username;
+
+        /**
+         * Create the representation of the
+         *  <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League's</a> player.
+         * @return not persisted {@link AppUser AppUser}
+         */
+        public AppUser createAppUser() {
+            return AppUser.builder()
+                    .email(email)
+                    .firstName(firstName)
+                    .lastName(lastName)
+                    .imageSource(imageSource)
+                    .username(username)
+                    .build();
+        }
     }
 
     /**
-     * Representation of the incoming League form <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League</a>
+     * Representation of the incoming League form
+     * <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League</a>
      */
     @Data
     private static class League {
-        @JsonProperty("id")
+        /**
+         * The id of the <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League's</a> league
+         */
+        @NonNull
         Long id;
 
         @JsonProperty("game_type")
+        @NonNull
         String gameType;
 
         @JsonProperty("image_source")
+        @NonNull
         String imageSource;
 
         @JsonProperty("league_admin_id")
+        @NonNull
         Long leagueAdminId;
 
         @JsonProperty("league_players")
+        @NonNull
         List<Player> leaguePlayers;
 
-        @JsonProperty("name")
+        @NonNull
         String name;
 
         @JsonProperty("number_of_finished_rounds")
-        int numberOfFinishedRounds;
+        @NonNull
+        Integer numberOfFinishedRounds;
 
         @JsonProperty("number_of_in_progress_rounds")
-        int numberOfInProgressRounds;
+        @NonNull
+        Integer numberOfInProgressRounds;
 
         @JsonProperty("number_of_rounds")
-        int numberOfRounds;
+        @NonNull
+        Integer numberOfRounds;
+
+        /**
+         * Create the representation of the
+         *  <a href="https://terraforming-mars-leagues.herokuapp.com/dashboard">Terraforming Mars League's</a> League.
+         * @return not persisted {@link LeagueDetail LeagueDetail}
+         */
+        public LeagueDetail createLeagueDetail() {
+            return LeagueDetail.builder()
+                    .name(name)
+                    .imageSource(imageSource)
+                    .numberOfRounds(numberOfRounds)
+                    .leagueAdmin(leaguePlayers.stream()
+                            .filter(player -> player.id.equals(leagueAdminId))
+                            .findFirst()
+                            .orElseThrow(() -> new NoSuchElementException("League admin not found!"))
+                            .createAppUser())
+                    .leagueId(id)
+                    .gameType(gameType)
+                    .build();
+        }
+
     };
 
     //TODO do something with the incoming players(are they already users, or not?).
