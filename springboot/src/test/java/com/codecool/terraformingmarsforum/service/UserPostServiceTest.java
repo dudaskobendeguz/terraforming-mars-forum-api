640dcbb71effb81b29921bd66449c3b780e18292
Benedek Halaj <bendihalaj@gmail.com>
2022-09-08 17:15:47 +0200

    test: add 'create user post' test cases covering 'has new id' and 'has timestamp'

--- springboot/src/test/java/com/codecool/terraformingmarsforum/service/UserPostServiceTest.java
+++ springboot/src/test/java/com/codecool/terraformingmarsforum/service/UserPostServiceTest.java
@@ -1,13 +1,15 @@
 package com.codecool.terraformingmarsforum.service;
 
 import com.codecool.terraformingmarsforum.model.UserPost;
+import com.codecool.terraformingmarsforum.repository.AppUserRepository;
 import com.codecool.terraformingmarsforum.repository.UserPostRepository;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -18,11 +20,13 @@ class UserPostServiceTest {
 
     @Mock
     private UserPostRepository userPostRepository;
+    @Mock
+    private AppUserRepository appUserRepository;
     private UserPostService userPostService;
 
     @BeforeEach
     public void init() {
-        userPostService = new UserPostService(userPostRepository);
+        userPostService = new UserPostService(userPostRepository, appUserRepository);
     }
 
     private List<UserPost> getUserPosts() {
@@ -33,10 +37,27 @@ class UserPostServiceTest {
     }
 
     @Test
-    public void getAllUserPosts_ReturnsAllUserPosts() {
+    public void getAllUserPosts_GetAllUserPosts_ReturnsAllUserPosts() {
         List<UserPost> expected = getUserPosts();
-        Mockito.when(userPostRepository.findAll()).thenReturn(expected);
+        when(userPostRepository.findAll()).thenReturn(expected);
         List<UserPost> actual = userPostService.getAllUserPosts();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createUserPost_NewUserPost_HasNewId() {
+        Long expected = 1L;
+        UserPost userPostWithNewId = UserPost.builder().id(expected).build();
+        UserPost userPost = UserPost.builder().build();
+        when(userPostRepository.save(userPost)).thenReturn(userPostWithNewId);
+        Long actual = userPostService.createUserPost(userPost).getId();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createUserPost_NewUserPost_HasTimestamp() {
+        UserPost userPost = UserPost.builder().build();
+        when(userPostRepository.save(userPost)).thenReturn(userPost);
+        assertNotNull(userPostService.createUserPost(userPost).getTimestamp());
     }
 }
