d70f704176344f3c0320c6345cdc7d49c5eabb9d
Zsezsu <zsezsu6@gmail.com>
2022-09-08 08:58:56 +0200

    docs: create method level javadoc for getAppUserByUsernameOrEmail() in AppUserService

--- springboot/src/main/java/com/codecool/terraformingmarsforum/service/AppUserService.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/service/AppUserService.java
@@ -17,6 +17,14 @@ public class AppUserService {
         return appUserRepository.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
+    /**
+     * This method tries to get a user from the DB using username or email.
+     * Any valid username OR email is acceptable
+     * @param username of the user
+     *                 OR
+     * @param email of the user
+     * @return AppUser Object if username or email is exists, throws NoSuchElementException anyway
+     */
     public AppUser getAppUserByUsernameOrEmail(String username, String email) {
         return appUserRepository.findAppUserByUsernameOrEmail(username, email).orElseThrow(NoSuchElementException::new);
     }
