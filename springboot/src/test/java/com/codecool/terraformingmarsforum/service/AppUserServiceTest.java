dc26b3af720602568899810cf2631a86cfc57be2
Zsezsu <zsezsu6@gmail.com>
2022-09-08 09:20:03 +0200

    test: add AppUserServiceTest file, implement getAppUserById_ValidId_returnsAppUser() test method

---
+++ springboot/src/test/java/com/codecool/terraformingmarsforum/service/AppUserServiceTest.java
@@ -0,0 +1,52 @@
+package com.codecool.terraformingmarsforum.service;
+
+import com.codecool.terraformingmarsforum.model.AppUser;
+import com.codecool.terraformingmarsforum.repository.AppUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AppUserServiceTest {
+
+    @Mock
+    private AppUserRepository appUserRepository;
+    private AppUserService appUserService;
+
+    @BeforeEach
+    public void init() {
+        appUserService = new AppUserService(appUserRepository);
+    }
+
+    @Test
+    void getAppUserById_ValidId_returnsAppUser() {
+        AppUser expectedTestUser = AppUser.builder()
+                .username("TesztElek")
+                .firstName("Teszt")
+                .lastName("Elek")
+                .email("tesztelek@gmail.com")
+                .password("teszt")
+                .imgSource("fb/img/tesztelek_profile_pic")
+                .timestamp(new Date())
+                .build();
+
+        Mockito.when(appUserRepository.findById(1L)).thenReturn(Optional.ofNullable(expectedTestUser));
+
+        AppUser actualTestUser = appUserService.getAppUserById(1L);
+
+        assertEquals(expectedTestUser, actualTestUser);
+    }
+
+
+    @Test
+    void getAppUserByUsernameOrEmail() {
+    }
+}
