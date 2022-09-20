55544c20bda7fc928705ed813d9b1252fe665c9c
benedekhalaj <bendihalaj@gmail.com>
2022-09-20 12:33:43 +0200

    chore: add 'get all posts' in 'post service'

--- springboot/src/main/java/com/codecool/terraformingmarsforum/service/PostService.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/service/PostService.java
@@ -1,10 +1,23 @@
 package com.codecool.terraformingmarsforum.service;
 
+import com.codecool.terraformingmarsforum.model.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
+    private final UserPostService userPostService;
+    private final LeaguePostService leaguePostService;
+
+    public List<Post> getAllPosts() {
+        List<Post> posts = new ArrayList<>();
+        posts.addAll(userPostService.getAllUserPosts());
+        posts.addAll(leaguePostService.findAll());
+        return posts;
+    }
 }
