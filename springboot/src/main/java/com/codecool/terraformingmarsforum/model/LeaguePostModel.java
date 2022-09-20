11b6375116ce3aee9f049656a48c5df851c78a72
benedekhalaj <bendihalaj@gmail.com>
2022-09-20 12:32:06 +0200

    chore: add 'post' marker interface

--- springboot/src/main/java/com/codecool/terraformingmarsforum/model/LeaguePostModel.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/model/LeaguePostModel.java
@@ -14,7 +14,7 @@ import java.util.Date;
 @Data
 @Builder
 @AllArgsConstructor
-public class LeaguePostModel {
+public class LeaguePostModel implements Post {
     Long leagueId;
     String description;
     Date timestamp;
