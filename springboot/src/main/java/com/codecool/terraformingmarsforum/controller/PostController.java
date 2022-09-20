55ecc0d9683d0422eea9f187c2ce47c8d13a3e03
benedekhalaj <bendihalaj@gmail.com>
2022-09-20 12:33:57 +0200

    feat: add 'post controller' with 'get all posts' route

---
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.codecool.terraformingmarsforum.controller;
+
+import com.codecool.terraformingmarsforum.model.Post;
+import com.codecool.terraformingmarsforum.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+}
