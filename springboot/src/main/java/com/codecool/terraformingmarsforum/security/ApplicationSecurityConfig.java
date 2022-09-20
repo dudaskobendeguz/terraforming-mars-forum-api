78ce277f2b545d708545734b7e66c16ada367ea5
Zsezsu <zsezsu6@gmail.com>
2022-09-20 16:01:11 +0200

    chore: implement daoAuthenticationProvider() in ApplicationSecurityConfig

--- springboot/src/main/java/com/codecool/terraformingmarsforum/security/ApplicationSecurityConfig.java
+++ springboot/src/main/java/com/codecool/terraformingmarsforum/security/ApplicationSecurityConfig.java
@@ -1,23 +1,43 @@
 package com.codecool.terraformingmarsforum.security;
 
+import com.codecool.terraformingmarsforum.service.AppUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    private final PasswordEncoder passwordEncoder;
+    private final AppUserService appUserService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").hasRole(Role.USER.name())
+                .antMatchers("/").hasRole("USER")
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 }
